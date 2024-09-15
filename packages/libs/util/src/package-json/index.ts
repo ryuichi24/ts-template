93115a02ec2649b3624777379b322e82cdf2c2c8
ryuichi24 <ryuichi.nishi24@gmail.com>
2024-09-16 02:00:12 +0900

    feat: update hitotsu

--- packages/libs/util/src/package-json/index.ts
+++ packages/libs/util/src/package-json/index.ts
@@ -11,10 +11,7 @@ export namespace PackageJSON {
     license: "MIT";
     type: "module" | "commonjs";
     main: string;
-    exports: Record<
-      "." | `./${string}`,
-      Record<"import" | "require", Record<"types" | "default", string>>
-    >;
+    exports: Record<string, Record<string, Record<string, string>>>;
     dependencies: Record<string, string>;
     devDependencies: Record<string, string>;
     publishMeta: {
@@ -30,6 +27,7 @@ export namespace PackageJSON {
         };
       };
     };
+    bin?: Record<string, string>;
   };
 }
 
@@ -49,11 +47,55 @@ export class PackageJSON {
     return this._props.publishMeta;
   }
 
+  public addScript(name: string, command: string) {
+    if (!this._props.scripts) {
+      this._props.scripts = {};
+    }
+    this._props.scripts[name] = command;
+  }
+
   public addDependency(name: string, version: string) {
     this._props = {
       ...this._props,
       dependencies: { ...this._props.dependencies, [name]: version },
     };
+  }
+
+  public addDevDependency(name: string, version: string) {
+    this._props = {
+      ...this._props,
+      devDependencies: { ...this._props.devDependencies, [name]: version },
+    };
+  }
+
+  public addMain(mainJsPath: string) {
+    this._props.main = mainJsPath;
+  }
+
+  public addExport(exports: {
+    path: string;
+    moduleImportKey: "import" | "require";
+    typesPath: string;
+    defaultPath: string;
+  }) {
+    if (!this._props.exports) {
+      this._props.exports = {};
+    }
+    if (!Object.hasOwn(this._props.exports, exports.path)) {
+      this._props.exports[exports.path] = {};
+    }
+    if (!Object.hasOwn(this._props.exports[exports.path], exports.moduleImportKey)) {
+      this._props.exports[exports.path][exports.moduleImportKey] = {};
+    }
+    this._props.exports[exports.path][exports.moduleImportKey]["types"] = exports.typesPath;
+    this._props.exports[exports.path][exports.moduleImportKey]["default"] = exports.defaultPath;
+  }
+
+  public addBin(command: string, binPath: string) {
+    if (!this._props.bin) {
+      this._props.bin = {};
+    }
+    this._props.bin[command] = binPath;
   }
 
   public toObj(): Partial<PackageJSON.Props> {
@@ -74,25 +116,13 @@ export function readPackageJsonFile(path: string) {
 }
 
 export function isNativeModule(packageJson: PackageJSON.Props) {
-  const NATIVE_BUILD_TOOLS = [
-    "bindings",
-    "node-addon-api",
-    "prebuild",
-    "nan",
-    "node-pre-gyp",
-    "node-gyp-build",
-  ];
+  const NATIVE_BUILD_TOOLS = ["bindings", "node-addon-api", "prebuild", "nan", "node-pre-gyp", "node-gyp-build"];
   if (!packageJson.dependencies && !packageJson.devDependencies) {
-    console.log(
-      `Cannot validate the package since there is no "dependencies" nor "devDependencies"`
-    );
+    console.log(`Cannot validate the package since there is no "dependencies" nor "devDependencies"`);
     return false;
   }
 
   return NATIVE_BUILD_TOOLS.some((tool) => {
-    return (
-      Reflect.has(packageJson.dependencies ?? {}, tool) ||
-      Reflect.has(packageJson.devDependencies ?? {}, tool)
-    );
+    return Reflect.has(packageJson.dependencies ?? {}, tool) || Reflect.has(packageJson.devDependencies ?? {}, tool);
   });
 }
--- packages/tools/hitotsu/src/index.js
+++ packages/tools/hitotsu/src/index.js
@@ -9,11 +9,7 @@ program
   .command("add <pathToModule>")
   .description("Add a new module to the workspace.")
   .option("-cjs, --commonjs", "Commonjs flag", false)
-  .option(
-    "-t, --moduletype <moduleType>",
-    "type of module (app, cli, lib, empty)",
-    "lib"
-  )
+  .option("-t, --moduletype <moduletype>", "type of module (app, cli, lib, empty)", "lib")
   .action((pathToModule, options) => {
     const fullPathToNewModule = path.resolve(pathToModule);
 
@@ -26,9 +22,7 @@ program
 
     // get root package.json
     const rootPackageJsonPath = path.resolve("package.json");
-    const rootPackageJson = JSON.parse(
-      fs.readFileSync(rootPackageJsonPath, "utf8")
-    );
+    const rootPackageJson = JSON.parse(fs.readFileSync(rootPackageJsonPath, "utf8"));
     const rootProjectName = rootPackageJson.name;
 
     const fullNewModuleName = `@${rootProjectName}/${newModuleName}`;
@@ -38,39 +32,114 @@ program
       name: fullNewModuleName,
       author: rootPackageJson.author,
       type: options.commonjs ? "commonjs" : "module",
-      exports: {
-        ".": {
-          import: {
-            types: "./dist/esm/index.d.ts",
-            default: "./dist/esm/index.js",
-          },
-          require: {
-            types: "./dist/cjs/index.d.ts",
-            default: "./dist/cjs/index.js",
-          },
-        },
-      },
-      main: "./dist/cjs/index.js",
-      scripts: {
-        build: "pnpm build:esm & pnpm build:cjs",
-        "build:esm":
-          'tsc -p tsconfig.json && fjs make ./dist/esm/package.json -c \'{"type":"module"}\'',
-        "build:cjs":
-          'tsc -p tsconfig.cjs.json && fjs make ./dist/cjs/package.json -c \'{"type":"commonjs"}\'',
-      },
-      type: "module",
+      private: true,
     });
 
-    if (options.moduleType === "app") {
-      //
+    if (options.moduletype === "app") {
+      packageJson.addScript("start", "");
+      packageJson.addScript("dev", "");
+      packageJson.addScript("build", "");
+
+      packageJson.addMain("./dist/index.js");
+
+      packageJson.addDevDependency("typescript", "^5.4.5");
+
+      const tsconfig = JSON.stringify(
+        {
+          extends: "@ts-template/tsconfig/node",
+          compilerOptions: {
+            outDir: "dist",
+          },
+          include: ["src"],
+        },
+        null,
+        2,
+      );
+      const tsconfigPath = path.join(fullPathToNewModule, "tsconfig.json");
+      fs.writeFileSync(tsconfigPath, tsconfig, "utf8");
     }
 
-    if (options.moduleType === "cli") {
-      //
+    if (options.moduletype === "cli") {
+      packageJson.addBin("cli", "dist/index.js");
+      packageJson.addDependency("commander", "^12.1.0");
+      packageJson.addDevDependency("@ts-template/tsconfig", "workspace:*");
+      packageJson.addDevDependency("@types/node", "^20.11.20");
+      packageJson.addDevDependency("typescript", "^5.4.5");
+      packageJson.addScript("build", "tsc -p tsconfig.json");
+      packageJson.addScript("postinstall", "pnpm build");
+      const tsconfig = JSON.stringify(
+        {
+          extends: "@ts-template/tsconfig/node",
+          compilerOptions: {
+            outDir: "dist",
+          },
+          include: ["src"],
+        },
+        null,
+        2,
+      );
+      const tsconfigPath = path.join(fullPathToNewModule, "tsconfig.json");
+      fs.writeFileSync(tsconfigPath, tsconfig, "utf8");
     }
 
-    if (options.moduleType === "lib") {
+    if (options.moduletype === "lib") {
+      packageJson.addDevDependency("@ts-template/tsconfig", "workspace:*");
+      packageJson.addDevDependency("@ts-template/type-util", "workspace:*");
+      packageJson.addDevDependency("@ts-template/tsbuild", "workspace:*");
       //
+      packageJson.addDevDependency("@types/node", "^20.11.20");
+      packageJson.addDevDependency("typescript", "^5.4.5");
+
+      packageJson.addScript("build", "pnpm build:esm & pnpm build:cjs");
+      packageJson.addScript("build:esm", "tsb tsconfig.json -t esm");
+      packageJson.addScript("build:cjs", "tsb tsconfig.cjs.json -t cjs");
+
+      packageJson.addMain("./dist/cjs/index.js");
+      packageJson.addExport({
+        path: ".",
+        moduleImportKey: "import",
+        typesPath: "./dist/esm/index.d.ts",
+        defaultPath: "./dist/esm/index.js",
+      });
+
+      packageJson.addExport({
+        path: ".",
+        moduleImportKey: "require",
+        typesPath: "./dist/cjs/index.d.ts",
+        defaultPath: "./dist/cjs/index.js",
+      });
+
+      const esmTsconfig = JSON.stringify(
+        {
+          display: "@ts-template/js-utils ESM",
+          extends: "@ts-template/tsconfig/node.lib",
+          compilerOptions: {
+            outDir: "./dist/esm",
+          },
+          include: ["src"],
+        },
+        null,
+        2,
+      );
+
+      const cjsTsconfig = JSON.stringify(
+        {
+          display: "ts-template/js-utils CJS",
+          extends: "@ts-template/tsconfig/node.lib.cjs",
+          compilerOptions: {
+            outDir: "./dist/cjs",
+          },
+          include: ["src"],
+        },
+        null,
+        2,
+      );
+
+      const esmTsconfigPath = path.join(fullPathToNewModule, "tsconfig.json");
+      fs.writeFileSync(esmTsconfigPath, esmTsconfig, "utf8");
+
+      const cjsTsconfigPath = path.join(fullPathToNewModule, "tsconfig.cjs.json");
+      fs.writeFileSync(cjsTsconfigPath, cjsTsconfig, "utf8");
     }
 
     if (options.moduleType === "empty") {
@@ -82,11 +151,7 @@ program
 
     // make README file
     const newReadmePath = path.join(fullPathToNewModule, "README.md");
-    fs.writeFileSync(
-      newReadmePath,
-      `<h1 align="center">${fullNewModuleName}</h1>`,
-      "utf8"
-    );
+    fs.writeFileSync(newReadmePath, `<h1 align="center">${fullNewModuleName}</h1>`, "utf8");
   });
 
 program.parse(process.argv);
