cef0d30e9616c1ae348594a2b5485fe4a1a6550c
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-03-16 22:07:31 +0100

    chore: reformat log message for ElectronLogStrategy

--- apps/desktop/main/src/util/logger.ts
+++ apps/desktop/main/src/util/logger.ts
@@ -5,11 +5,12 @@ import electronLogger from "electron-log";
 class ElectronLogStrategy extends Logger.BaseLogStrategy {
   log({ level, message, loggerName, logPrefix }: Logger.LogPayload): void {
     const logLevel = Logger.LogLevel[level].toLowerCase() as "info" | "warn" | "error" | "debug" | "fatal";
+    const logMessage = `${logPrefix} ${message}`;
     if (logLevel === "fatal") {
-      electronLogger.error(`[+FATAL] ${message}`);
+      electronLogger.error(`[FATAL] ${logMessage}`);
       return;
     }
-    electronLogger[logLevel](message);
+    electronLogger[logLevel](logMessage);
   }
 }
 
