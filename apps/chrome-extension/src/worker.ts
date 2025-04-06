5f00a7b99eb3f1ff2b9490fba2702f7cb876211f
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-04-07 00:55:30 +0200

    feat: add some example codes for chrome extension

--- apps/chrome-extension/src/worker.ts
+++ apps/chrome-extension/src/worker.ts
@@ -10,6 +10,47 @@ chrome.action.onClicked.addListener((tab) => {
   }
 });
 
+chrome.webRequest.onCompleted.addListener(
+  async (details) => {
+    const url = details.url;
+    const domain = new URL(url).hostname;
+    const requestLog = {
+      id: crypto.randomUUID(),
+      url,
+      domain,
+      timestamp: Date.now(),
+      type: details.type,
+    };
+
+    console.log("Request completed:", requestLog);
+  },
+  {
+    urls: ["<all_urls>"],
+    types: ["main_frame"],
+  },
+  [],
+);
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (!changeInfo.url || !tab.url) {
+    return;
+  }
+  
+  const url = tab.url;
+  const domain = new URL(url).hostname;
+  const requestLog = {
+    id: crypto.randomUUID(),
+    url,
+    domain,
+    timestamp: Date.now(),
+    headers: {},
+    type: "spa_navigation",
+  };
+
+  console.log("Request completed:", requestLog);
+});
+
+// for development
 chrome.commands.onCommand.addListener((command) => {
   if (command === "reload") {
     console.log("Reloading the extension...");
