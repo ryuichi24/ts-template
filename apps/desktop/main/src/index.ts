30d7661027e2fea472a320afc7d79c52d0394821
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-04-22 13:35:18 +0200

    chore: change folder name

--- apps/desktop/main/src/index.ts
+++ apps/desktop/main/src/index.ts
@@ -1,18 +1,18 @@
 import path from "path";
 import Electron from "electron";
 import { ElectronFactory } from "./util/ElectronFactory.js";
-import { LoadMainWindowModule } from "./modules/load-main-window.module.js";
-import { CreateTrayModule } from "./modules/create-tray.module.js";
-import { OpenInBrowserModule } from "./modules/open-in-browser.module.js";
-import { StartChildProcessesModule } from "./modules/start-child-processes.module.js";
-import { CheckCurrentUpdateChannelModule } from "./modules/check-current-update-channel.module.js";
-import { ChangeUpdateChannelModule } from "./modules/change-update-channel.module.js";
-import { CheckAppVersionModule } from "./modules/check-app-version.module.js";
-import { checkForUpdatesModule } from "./modules/check-for-update.module.js";
-import { SetupAutoUpdaterModule } from "./modules/setup-auto-updater.module.js";
-import { SetupDeepLinkModule } from "./modules/setup-deep-link.module.js";
-import { HandleAppWindowFocusModule } from "./modules/handle-app-window-focus.module.js";
-import { HandleOSLevelEncryptionModule } from "./modules/handle-os-level-encryption.module.js";
+import { LoadMainWindowModule } from "./features/load-main-window.module.js";
+import { CreateTrayModule } from "./features/create-tray.module.js";
+import { OpenInBrowserModule } from "./features/open-in-browser.module.js";
+import { StartChildProcessesModule } from "./features/start-child-processes.module.js";
+import { CheckCurrentUpdateChannelModule } from "./features/check-current-update-channel.module.js";
+import { ChangeUpdateChannelModule } from "./features/change-update-channel.module.js";
+import { CheckAppVersionModule } from "./features/check-app-version.module.js";
+import { checkForUpdatesModule } from "./features/check-for-update.module.js";
+import { SetupAutoUpdaterModule } from "./features/setup-auto-updater.module.js";
+import { SetupDeepLinkModule } from "./features/setup-deep-link.module.js";
+import { HandleAppWindowFocusModule } from "./features/handle-app-window-focus.module.js";
+import { HandleOSLevelEncryptionModule } from "./features/handle-os-level-encryption.module.js";
 
 if (require("electron-squirrel-startup")) Electron.app.quit();
 
